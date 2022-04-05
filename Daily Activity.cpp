9f13bd554f0b29b71f9619157b43659c5c049fe1
JVValerio <103063086+JVValerio@users.noreply.github.com>
2022-04-05 10:28:52 -0700

    Add files via upload

---
+++ Daily Activity.cpp
@@ -0,0 +1,19 @@
+// Josh Valerio
+// Daily Activity 1
+
+//#include "pch.h"
+// I was not able to find a solution to the precompiled header error on line 4. I unable to generate one, so I just commented it out for now.
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout << "Name: Josh Valerio\n";
+	cout << "Hello World\n";
+
+	system("pause");
+
+	return 0;
+}
